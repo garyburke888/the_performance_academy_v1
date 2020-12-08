d45f72145e190213fbaac402529e7533c23abcfe
garyburke888 <garyburke888@gmail.com>
2020-12-08 15:01:12 +0000

    Customise admin

--- classes/admin.py
+++ classes/admin.py
@@ -2,7 +2,47 @@ from django.contrib import admin
 from .models import Class, Category, Setting, Day
 
 # Register your models here.
-admin.site.register(Class)
-admin.site.register(Category)
-admin.site.register(Setting)
-admin.site.register(Day)
+
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'age',
+        'setting',
+        'day',
+        'time',
+        'teacher',
+        'price',
+        'term',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class DayAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Class, ClassAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Setting, SettingAdmin)
+admin.site.register(Day, DayAdmin)
--- classes/models.py
+++ classes/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -15,6 +19,7 @@ class Category(models.Model):
 
 
 class Setting(models.Model):
+    
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -37,6 +42,10 @@ class Day(models.Model):
 
 
 class Class(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Classes'
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
