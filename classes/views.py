44a8e4c13c559ce408c33f06f8842f4b75e6d663
garyburke888 <garyburke888@gmail.com>
2020-12-08 15:14:42 +0000

    Add classes views and templates

--- classes/views.py
+++ classes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Class
 
 # Create your views here.
+
+
+def all_classes(request):
+    """ A view to show all classes, including sorting and search queries """
+
+    classes = Class.objects.all()
+
+    context = {
+        'classes': classes,
+    }
+
+    return render(request, 'classes/classes.html', context)
