c1d636612dc9ee0fc24fcbf1fa295abf3413ab7c
garyburke888 <garyburke888@gmail.com>
2020-12-15 12:34:44 +0000

    Add search functionality

--- classes/views.py
+++ classes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Class
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_classes(request):
     """ A view to show all classes, including sorting and search queries """
 
     classes = Class.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('classes'))
+                
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            classes = classes.filter(queries)
 
     context = {
         'classes': classes,
+        'search_term': query,
     }
 
     return render(request, 'classes/classes.html', context)
