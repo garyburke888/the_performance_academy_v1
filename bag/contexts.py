13232d03933dcc83f2cf09f340e5705677824b15
garyburke888 <garyburke888@gmail.com>
2020-12-23 09:50:12 +0000

    Update shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from classes.models import Class
 
 
 def bag_contents(request):
@@ -7,6 +9,17 @@ def bag_contents(request):
     bag_items = []
     total = 0
     class_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        a_class = get_object_or_404(Class, pk=item_id)
+        total += quantity * a_class.price
+        class_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'class': a_class,
+        })
 
     if class_count >= settings.DISCOUNT_THRESHOLD:
         discount = total * Decimal(settings.STANDARD_DISCOUNT_PERCENTAGE / 100)
