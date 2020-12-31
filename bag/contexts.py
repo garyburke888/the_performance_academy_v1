9b699a52c1bde091fd081a37b8b938d8c51a3bf5
garyburke888 <garyburke888@gmail.com>
2020-12-31 10:21:28 +0000

    Finish class day logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -11,22 +11,34 @@ def bag_contents(request):
     class_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        a_class = get_object_or_404(Class, pk=item_id)
-        total += quantity * a_class.price
-        class_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'class': a_class,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            a_class = get_object_or_404(Class, pk=item_id)
+            total += item_data * a_class.price
+            class_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'class': a_class,
+            })
+        else:
+            a_class = get_object_or_404(Class, pk=item_id)
+            for day, quantity in item_data['items_by_day'].items():
+                total += quantity * a_class.price
+                class_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'class': a_class,
+                    'day': day,
+                })
 
     if class_count >= settings.DISCOUNT_THRESHOLD:
         discount = total * Decimal(settings.STANDARD_DISCOUNT_PERCENTAGE / 100)
         discount_delta = 0
     else:
         discount = 0
-        discount_delta = class_count + 1
+        discount_delta = 2 - class_count
 
     grand_total = total - discount
 
--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    day = None
+    if 'class_day' in request.POST:
+        day = request.POST['class_day']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if day:
+        if item_id in list(bag.keys()):
+            if day in bag[item_id]['items_by_day'].keys():
+                bag[item_id]['items_by_day'][day] += quantity
+            else:
+                bag[item_id]['items_by_day'][day] = quantity
+        else:
+            bag[item_id] = {'items_by_day': {day: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
