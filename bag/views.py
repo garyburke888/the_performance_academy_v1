bcb6ea7fc4a13f9476fd06438ff28628d7eb7a0f
garyburke888 <garyburke888@gmail.com>
2020-12-31 14:24:46 +0000

    Add notification CSS, bag preview and additional messages

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from classes.models import Class
@@ -15,7 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    a_class = Class.objects.get(pk=item_id)
+    a_class = get_object_or_404(Class, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     day = None
@@ -27,13 +27,17 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if day in bag[item_id]['items_by_day'].keys():
                 bag[item_id]['items_by_day'][day] += quantity
+                messages.success(request, f'Updated day {day.upper()} {a_class.name} quantity to {bag[item_id]["items_by_day"][day]}')
             else:
                 bag[item_id]['items_by_day'][day] = quantity
+                messages.success(request, f'Added day {day.upper()} {a_class.name} to your bag')
         else:
             bag[item_id] = {'items_by_day': {day: quantity}}
+            messages.success(request, f'Added day {day.upper()} {a_class.name} to your bag')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
+            messages.success(request, f'Updated {a_class.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {a_class.name} to your bag')
@@ -45,6 +49,7 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
+    a_class = get_object_or_404(Class, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     day = None
     if 'class_day' in request.POST:
@@ -54,15 +59,19 @@ def adjust_bag(request, item_id):
     if day:
         if quantity > 0:
             bag[item_id]['items_by_day'][day] = quantity
+            messages.success(request, f'Updated day {day.upper()} {a_class.name} quantity to {bag[item_id]["items_by_day"][day]}')
         else:
             del bag[item_id]['items_by_day'][day]
             if not bag[item_id]['items_by_day']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed day {day.upper()} {a_class.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {a_class.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {a_class.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -72,6 +81,7 @@ def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
+        a_class = get_object_or_404(Class, pk=item_id)
         day = None
         if 'class_day' in request.POST:
             day = request.POST['class_day']
@@ -81,11 +91,14 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_day'][day]
             if not bag[item_id]['items_by_day']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed day {day.upper()} {a_class.name} from your bag')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {a_class.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
