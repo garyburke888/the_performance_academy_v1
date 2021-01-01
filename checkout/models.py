0fcd7d306ffa88af600491eb5d49a163f222d59c
garyburke888 <garyburke888@gmail.com>
2021-01-01 10:37:51 +0000

    Add forms, admin and signals

--- checkout/models.py
+++ checkout/models.py
@@ -60,8 +60,8 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
-    a_class = models.ForeignKey(Class, null=False, blank=False, on_delete=models.CASCADE)
-    class_day = models.CharField(max_length=9, null=True, blank=True) # Monday, Tuesday, Wednesday, Thursday, Friday
+    class_name = models.ForeignKey(Class, null=False, blank=False, on_delete=models.CASCADE)
+    day = models.CharField(max_length=9, null=True, blank=True) # Monday, Tuesday, Wednesday, Thursday, Friday
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
