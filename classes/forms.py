6eeb41a32f102a6d21e751e3dc864745b7156153
garyburke888 <garyburke888@gmail.com>
2021-01-07 16:12:27 +0000

    Add custom image widget field to add/edit class form

--- classes/forms.py
+++ classes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Class, Category
 
 
@@ -7,6 +8,8 @@ class ClassForm(forms.ModelForm):
     class Meta:
         model = Class
         fields = '__all__'
+
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
