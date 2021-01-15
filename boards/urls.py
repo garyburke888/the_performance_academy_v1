d94933bb86646274d226bf508c1c72d83132c1dc
garyburke888 <garyburke888@gmail.com>
2021-01-15 18:46:07 +0000

    Add pagination to boards

--- boards/urls.py
+++ boards/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.boards, name='boards'),
-    path('<int:pk>/', views.board_topics, name='board_topics'),
+    path('', views.BoardListView.as_view(), name='boards'),
+    path('<int:pk>/', views.TopicListView.as_view(), name='board_topics'),
     path('<int:pk>/new/', views.new_topic, name='new_topic'),
-    path('<int:pk>/topics/<int:topic_pk>/', views.topic_posts, name='topic_posts'),
+    path('<int:pk>/topics/<int:topic_pk>/', views.PostListView.as_view(), name='topic_posts'),
     path('<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
+    path('<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/',
+        views.PostUpdateView.as_view(), name='edit_post'),
+    path('<int:pk>', views.TopicListView.as_view(), name='board_topics'),
 ]
