c7200f76d778a8d29dd86a63c45c1ba6a33db1f7
garyburke888 <garyburke888@gmail.com>
2021-01-18 08:53:18 +0000

    Re-style boards edit_post page

--- boards/urls.py
+++ boards/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('<int:pk>/new/', views.new_topic, name='new_topic'),
     path('<int:pk>/topics/<int:topic_pk>/', views.PostListView.as_view(), name='topic_posts'),
     path('<int:pk>/topics/<int:topic_pk>/reply/', views.reply_topic, name='reply_topic'),
-    path('<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/',
-        views.PostUpdateView.as_view(), name='edit_post'),
+    path('<int:pk>/topics/<int:topic_pk>/posts/<int:post_pk>/edit/', views.PostUpdateView.as_view(), name='edit_post'),
     path('<int:pk>', views.TopicListView.as_view(), name='board_topics'),
 ]
