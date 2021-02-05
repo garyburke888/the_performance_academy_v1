1e2c025277642ab750fb3bf6ba67fe39aa301b61
garyburke888 <garyburke888@gmail.com>
2021-02-05 08:53:33 +0000

    Add and implement Django tests on posts view in Boards app

--- boards/test_boards_view.py
+++ boards/test_boards_view.py
@@ -7,19 +7,23 @@ from .views import BoardListView
 class BoardsTests(TestCase):
 
     def setUp(self):
+        """ Create a new test board """
         self.board = Board.objects.create(
             name='Django', description='Django board.')
         url = reverse('boards')
         self.response = self.client.get(url)
 
     def test_boards_view_status_code(self):
+        """ Test the boards view status code """
         self.assertEquals(self.response.status_code, 200)
 
     def test_boards_url_resolves_boards_view(self):
+        """ Test the boards url resolves to correct view """
         view = resolve('/boards/')
         self.assertEquals(view.func.view_class, BoardListView)
 
     def test_boards_view_contains_link_to_topics_page(self):
+        """ Test the boards view links to its topic page """
         board_topics_url = reverse(
             'board_topics', kwargs={'pk': self.board.pk})
         self.assertContains(
--- boards/test_topics_view.py
+++ boards/test_topics_view.py
@@ -7,23 +7,30 @@ from .views import TopicListView
 
 class BoardTopicsTests(TestCase):
     def setUp(self):
+        """ Creat a new test topic """
         Board.objects.create(name='Django', description='Django board.')
 
     def test_board_topics_view_success_status_code(self):
+        """ Test the topics view status code """
         url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
     def test_board_topics_view_not_found_status_code(self):
+        """ Test status code for a topic that doesn't exist
+        returns a 404 error """
         url = reverse('board_topics', kwargs={'pk': 99})
         response = self.client.get(url)
         self.assertEquals(response.status_code, 404)
 
     def test_board_topics_url_resolves_board_topics_view(self):
+        """ Test the topics url resolves to the correct view """
         view = resolve('/boards/1/')
         self.assertEquals(view.func.view_class, TopicListView)
 
     def test_board_topics_view_contains_navigation_links(self):
+        """ Test tpoics page contains links to boards page and
+        to add a new topic (page) """
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         homepage_url = reverse('boards')
         new_topic_url = reverse('new_topic', kwargs={'pk': 1})
