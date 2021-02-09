d7e066d9ea54232270dd9cd7321e723dd866b46f
garyburke888 <garyburke888@gmail.com>
2021-02-09 14:55:50 +0000

    Email sending error, amend settings.py

--- the_performance_academy/settings.py
+++ the_performance_academy/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'bg(2f10_q)-)!w2g8^#ek9^f(s)nklsi34*s4ws-x!a^pf=edt')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
