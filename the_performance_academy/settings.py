900aa89a8dcbbd2e583e5f7a6b8988c4cd43097c
garyburke888 <garyburke888@gmail.com>
2021-02-18 19:34:50 +0000

    Set debug to false

--- the_performance_academy/settings.py
+++ the_performance_academy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ['the-performance-academy.herokuapp.com', 'localhost']
 
